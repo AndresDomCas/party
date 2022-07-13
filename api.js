9430a38593eb001de64fb108a32319e54c0ba41b
AndresDomCas <t.d.w.p_ff@hotmail.com>
2022-07-13 11:16:33 -0500

    Fin

--- api.js
+++ api.js
@@ -1,8 +1,8 @@
 var count_particles, stats, update;
-  stats = new Stats;
-  stats.setMode(0);
-  count_particles = document.querySelector('.js-count-particles');
-  update = function() {
-    requestAnimationFrame(update);
-  };
+stats = new Stats;
+stats.setMode(0);
+count_particles = document.querySelector('.js-count-particles');
+update = function () {
   requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
